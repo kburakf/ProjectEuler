f90bd74cb90e380413a061351a02c78278675da3
kürşat burak farız <kburakfariz@gmail.com>
2019-09-05 23:46:06 +0300

    problem 50 solved

--- controller/solutions.js
+++ controller/solutions.js
@@ -797,17 +797,36 @@ module.exports.q36 = () => {
 // SOLUTION 50
 
 module.exports.q50 = () => {
-    function isPrime(number) {
-        let sum = 0
-        for (let i = 2; i <= number; i++) {
-            let prime = true
-            for (let j = 2; j < i; j++)
-                if (i % j == 0) prime = false
-            if (prime) sum += i
+    const limit = 1000000
+    const notPrime = new Uint8Array(limit);
+    const primes = [];
+
+    notPrime[0] = notPrime[1] = 1;
+
+    for (let i = 2; i < limit; i++) {
+        if (notPrime[i] === 0) {
+            primes.push(i);
+            for (let j = 2 * i; j < limit; j += i) {
+                notPrime[j] = 1;
+            }
         }
-        console.log(sum)
     }
-    isPrime(13)
+    let maxSum = 0;
+    let maxRun = -1;
+    for (let i = 0; i < primes.length; i++) {
+
+        let sum = 0;
+        for (let j = i; j < primes.length; j++) {
+            sum += primes[j];
+            if (sum > limit)
+                break;
+            if (!notPrime[sum] && sum > maxSum && j - i > maxRun) {
+                maxRun = j - i;
+                maxSum = sum;
+            }
+        }
+    }
+    console.log(maxSum)
 }
 
 // SOLUTION 67 
