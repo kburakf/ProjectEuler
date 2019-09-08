aacf34bd63049756e3806fe36bead966bc69bb40
kürşat burak farız <kburakfariz@gmail.com>
2019-09-08 20:54:10 +0300

    problem 39 almost done

--- controller/solutions.js
+++ controller/solutions.js
@@ -777,6 +777,25 @@ module.exports.q36 = () => {
     console.log(sum)
 }
 
+// SOLUTION 39
+
+module.exports.q39 = () => {
+    function p39(p) {
+        let count = 0,
+            c2, c
+        for (let a = 1; a <= p; a++) {
+            for (let b = a; b <= p; b++) {
+                c2 = a ** 2 + b ** 2
+                c = Math.sqrt(c2)
+                if (a + b + c <= p)
+                    count++
+            }
+        }
+        console.log(count)
+    }
+    p39(1000)
+}
+
 // SOLUTION 44
 
 module.exports.q44 = () => {
