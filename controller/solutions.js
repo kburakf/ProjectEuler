ff19cdd7d9b04404cbdb43ac30e83e92e7a5b23d
kürşat burak farız <kburakfariz@gmail.com>
2019-09-09 20:33:25 +0300

    problem 34 and 37 will done

--- controller/solutions.js
+++ controller/solutions.js
@@ -777,24 +777,45 @@ module.exports.q36 = () => {
     console.log(sum)
 }
 
+// SOLUTION 34
+
+module.exports.q34 = () => {
+    
+}
+
+// SOLUTION 37
+
+// module.exports.q37 = () => {
+//     let sum =0
+//     for(let i=1001;i<9999;i+=2){
+//         let bol = i.toString().split("")
+//         for(let j = 0;j<bol.length;j++){
+//             let conv = parseInt(bol[j])
+//             for(let k=9;0<k;k--){
+
+//             }
+//         }
+//     }
+// }
+
 // SOLUTION 39
 
-module.exports.q39 = () => {
-    function p39(p) {
-        let count = 0,
-            c2, c
-        for (let a = 1; a <= p; a++) {
-            for (let b = a; b <= p; b++) {
-                c2 = a ** 2 + b ** 2
-                c = Math.sqrt(c2)
-                if (a + b + c <= p)
-                    count++
-            }
-        }
-        console.log(count)
-    }
-    p39(1000)
-}
+// module.exports.q39 = () => {
+//     function p39(p) {
+//         let count = 0,
+//             c2, c
+//         for (let a = 1; a <= p; a++) {
+//             for (let b = a; b <= p; b++) {
+//                 c2 = a ** 2 + b ** 2
+//                 c = Math.sqrt(c2)
+//                 if (a + b + c <= p)
+//                     count++
+//             }
+//         }
+//         console.log(count)
+//     }
+//     p39(1000)
+// }
 
 // SOLUTION 44
 
