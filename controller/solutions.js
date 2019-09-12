ae4b0a7bf5aa41ce29ce4e67a8067ab11e8e7829
kürşat burak farız <kburakfariz@gmail.com>
2019-09-13 00:21:39 +0300

    problem 35 solved <3

--- controller/solutions.js
+++ controller/solutions.js
@@ -789,14 +789,41 @@ module.exports.q34 = () => {
     console.log(facto)
 }
 
+// SOLUTION 35
+
+module.exports.q35 = () => {
+    function isPrime(number) {
+        for (let i = 2; i < number / 2; i++) {
+            if (number % i == 0) return false
+        }
+        return true
+    }
+
+    function rotationPrime(number) {
+        let conv = String(number)
+
+        for (let i = 1; i < conv.length; i++) {
+            conv = conv.slice(-1) + conv.slice(0, conv.length - 1)
+            if (!isPrime(parseInt(conv))) return false
+        }
+        return true
+    }
+    let count = 13
+
+    for (let i = 101; i < 1000000; i += 2)
+        if (isPrime(i) && rotationPrime(i))
+            count++
+
+    console.log(count)
+}
 // SOLUTION 36
 
 module.exports.q36 = () => {
     let sum = 0
-    for (let i = 0; i < 1000000; i++) {
+    for (let i = 0; i < 1000000; i++)
         if (i.toString(2).split("").reverse().join("") === i.toString(2) && i.toString().split("").reverse().join("") === i.toString())
             sum += i
-    }
+
     console.log(sum)
 }
 
