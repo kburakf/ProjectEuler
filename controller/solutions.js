9537ad347a9dfb855d6ea3fe5c6dc3199e23d21e
kürşat burak farız <kburakfariz@gmail.com>
2019-09-16 21:46:55 +0300

    still working on 37

--- controller/solutions.js
+++ controller/solutions.js
@@ -835,25 +835,34 @@ module.exports.q36 = () => {
 // SOLUTION 37
 
 module.exports.q37 = () => {
+    function isPrime(number) {
+        for (let i = 2; i < number / 2; i++) {
+            if (number % i == 0) return false
+        }
+        return true
+    }
     let sum = 0,
         count = 0
-
-    function isPrime(num) {
-        for (let i = 2; i < num; i++) {
-            if (i % num == 0) return false
-            return true
-        }
-        let i = 13
-        while (count != 11) {
-            if (isPrime(i)) {
-                let conv = String(i)
-                for (let j = 0; j < conv.length; j++) {
-                    
+    let i = 13
+    while (count != 11) {
+        if (isPrime(i)) {
+            let beString = i.toString()
+            let beRevString = beString.split("").reverse().join("")
+            for (let j = 0; j < beString.length; j++) {
+                let after = beString.substring(0, j + 1)
+                let after2 = beRevString.substring(0, j + 1)
+                if (isPrime(parseInt(after)) && isPrime(parseInt(after2))) {
+                    sum += i
+                    count++
                 }
             }
         }
+        i++
     }
+    console.log(sum)
+
 }
+
 // SOLUTION 39
 
 // module.exports.q39 = () => {
